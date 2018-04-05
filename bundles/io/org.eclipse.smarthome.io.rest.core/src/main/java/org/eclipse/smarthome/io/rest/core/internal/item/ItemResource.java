071a49e5e82dde1232b98ff74203ac79fd3da590
Antonio SV <jesus.antoniosv@gmail.com>
2018-04-05 18:01:33 +0300

    deleted conflicting line

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/internal/item/ItemResource.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/internal/item/ItemResource.java
@@ -174,7 +174,6 @@ public class ItemResource implements RESTResource {
     @GET
     @RolesAllowed({ Role.USER, Role.ADMIN })
     @Produces(MediaType.APPLICATION_JSON)
-<<<<<<< HEAD:bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/internal/item/ItemResource.java
     @ApiOperation(value = "Get all available items.", response = EnrichedItemDTO.class, responseContainer = "List", authorizations = {
             @Authorization(value = "oauth2", scopes = {
                     @AuthorizationScope(scope = "items", description = "List items and read their states") }) })
