b9fdf2c9af1117f98557ad2f3c80ab4dbfb9952f
Antonio SV <jesus.antoniosv@gmail.com>
2018-05-05 13:15:21 +0300

    unworking custom http context -- problems with dependencies

--- bundles/auth/org.eclipse.smarthome.auth.jwt/src/org/eclipse/smarthome/auth/jwt/internal/CustomHttpContext.java
+++ bundles/auth/org.eclipse.smarthome.auth.jwt/src/org/eclipse/smarthome/auth/jwt/internal/CustomHttpContext.java
@@ -18,6 +18,13 @@ package org.eclipse.smarthome.auth.jwt.internal;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Base64;
+import java.text.ParseException;
+import java.util.StringTokenizer;
+
+import java.security.interfaces.*;
+import javax.crypto.*;
+import java.security.*;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.*;
@@ -38,7 +45,8 @@ public class CustomHttpContext implements HttpContext {
     private static final Logger LOG = LoggerFactory.getLogger(CustomHttpContext.class);
 
     private Bundle bundle;
-
+    private static KeyPair kp = null;
+    
     CustomHttpContext(Bundle bundle) {
 	this.bundle = bundle;
     }    
@@ -47,8 +55,62 @@ public class CustomHttpContext implements HttpContext {
     public boolean handleSecurity(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException {
         LOG.info("Handling security now...");
+	LOG.info("Generating jwt");
+	String token = generateJwt("baidi", "alola");
+	// if(verifyJwt(token)) {
+	//     LOG.info("verification SUCCESS");
+	// } else {
+	//     LOG.info("verification FAILURE");
+	// }
         return true;
     }
+
+    protected KeyPair getKeyPair() {
+	if(kp == null) {
+	    try {
+	    KeyPairGenerator keyGenerator = null;
+	    keyGenerator = KeyPairGenerator.getInstance("RSA");
+	    keyGenerator.initialize(1024);
+	    kp = keyGenerator.genKeyPair();
+	    } catch(NoSuchAlgorithmException e) {}
+	}
+	return kp;
+    }
+    
+    protected String generateJwt(String username, String claim)  { // throws JOSEException {
+	RSAPublicKey publicKey = (RSAPublicKey)getKeyPair().getPublic();
+	RSAPrivateKey privateKey = (RSAPrivateKey)getKeyPair().getPrivate();
+	// JWSSigner signer = new RSASSASigner(privateKey);
+
+	// // what's the key ID?
+	// JWSObject jwsObject = new JWSObject(
+    	// 				    new JWSHeader.Builder(JWSAlgorithm.RS256).keyID("123").build(),
+    	// 				    new Payload(username));
+    	// jwsObject.sign(signer);
+	String token = "";
+	//token = jwsObject.serialize();	
+	return token;
+	
+    }
+
+    // protected boolean verifyJwt(String token) throws JOSEException{
+    // 	boolean valid = false;
+    // 	RSAPublicKey publicKey = (RSAPublicKey)getKeyPair().getPublic();
+    // 	JWSObject jwsObject = null;
+
+    // 	if(token != null && !token.isEmpty()) {
+    // 	    try {
+    // 		jwsObject = JWSObject.parse(token);
+    // 	    } catch(ParseException e) { LOG.info("problem parsing token: " + token); }
+    // 	}
+    // 	LOG.info("Attempting to verify token...");
+    // 	JWSVerifier verifier = new RSASSAVerifier(publicKey);
+    // 	if(jwsObject != null) {
+    // 	    valid = jwsObject.verify(verifier);
+    // 	    LOG.info("Token Payload: " + jwsObject.getPayload().toString());
+    // 	}	
+    // 	return valid;
+    // }           
 
     @Override
     public URL getResource(final String name) {
